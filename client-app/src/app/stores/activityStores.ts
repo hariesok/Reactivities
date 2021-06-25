c14ea73a7342f3eaf894494b4a467dbefb2213d4
hariesok <108.haries@gmail.com>
2021-06-25 10:00:57 +0700

    Last commit before using single page components

--- client-app/src/app/stores/activityStores.ts
+++ client-app/src/app/stores/activityStores.ts
@@ -23,14 +23,39 @@ export default class ActivityStore {
         try {
             const activities = await agent.Activities.list()
             activities.forEach(activity => {
-                activity.date = activity.date.split('T')[0]
-                this.activityRegistry.set(activity.id, activity)
+                this.setActivity(activity)
             })
             this.setLoadingInitial(false)
         } catch (error) {
             console.log(error)
             this.loadingInitial = false
         }
+    }
+
+    loadActivity = async (id: string) => {
+        let activity = this.getActivity(id)
+        if(activity) {
+            this.selectedActivity = activity
+        } else {
+            this.loadingInitial = true
+            try {
+                activity = await agent.Activities.details(id)
+                this.setActivity(activity)
+                this.setLoadingInitial(false)
+            } catch (error) {
+                console.log(error)
+                this.setLoadingInitial(false)
+            }
+        }
+    }
+
+    private setActivity = (activity: Activity) => {
+        activity.date = activity.date.split('T')[0]
+        this.activityRegistry.set(activity.id, activity)
+    }
+
+    private getActivity = (id: string) => {
+        return this.activityRegistry.get(id)
     }
 
     setLoadingInitial = (state: boolean) => {
