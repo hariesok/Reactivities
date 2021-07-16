49131d513bb3b11d50596f6c05c32fdafb0fd578
hariesok <108.haries@gmail.com>
2021-07-16 08:11:14 +0700

    End of section 8

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,19 +8,27 @@ import { Route, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
- 
+
 function App() {
   const location = useLocation() // will generate location key
-  
+
   return (
     <>
-      <NavBar />
-      <Container style={{marginTop: '7em'}}>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/activities' component={ActivityDashboard} />
-        <Route path='/activities/:id' component={ActivityDetails} />
-        <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-      </Container>
+      <Route exact path='/' component={HomePage} />
+      <Route
+        path={'/(.+)'}
+        render={() => (
+          <>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+            </Container>
+          </>
+        )}
+      />
+
     </>
   );
 }
